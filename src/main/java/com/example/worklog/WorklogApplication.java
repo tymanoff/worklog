ffc20021b7a9323c94ceede8182a30cdc2278819
tymanoff <djdkdancik@gmail.com>
2022-06-26 12:45:32 +0400

    add UserData, add WorkLog, creat message worklog jira

--- src/main/java/com/example/worklog/WorklogApplication.java
+++ src/main/java/com/example/worklog/WorklogApplication.java
@@ -4,14 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorklogApplication
-{
-
-    public static void main(String[] args)
-    {
+public class WorklogApplication {
+    public static void main(String[] args) {
         SpringApplication.run(WorklogApplication.class, args);
-
-
-
     }
 }
--- src/main/java/com/example/worklog/jira/Consts.java
+++ src/main/java/com/example/worklog/jira/Consts.java
@@ -8,7 +8,13 @@ public class Consts {
     public static final String START_MSG = "Этот бот предназначен для сервиса Jira. Что бы узнать возможности бота, напишите /help";
     public static final String UNKNOWN_MSG = "Что вы от меня хотите?  Напишите /start" ;
     public static final String DONE = "Выполнено" ;
-    public static final String HELP = "В данный момент времени есть только одна комадна /worklog" ;
+    public static final String HELP = "В данный момент времени есть только одна комадна /issue" ;
+    public static final String ISSUE = "Напишите issue";
+    public static final String WORKLOG = "Напишите время в формате <Hh Mm>, <Hh>, <Mm>";
+    public static final String TEXT = "Напишите /worklog";
+
+
+
 
 
 
